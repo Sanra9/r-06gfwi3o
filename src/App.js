96c2e1ea2183d91603ff48fa1781c4046e9c6f47
sanra9 <sanrra999@gmail.com>
2019-05-26 22:42:29 -0500

    jé

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome.js';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       names: ["Juan", "Pedro", "Germán"]
     }
   }
 
   render() {
+    const names = ['Juan', 'Pedro', 'Germán']
     return (
       <div>
-        {this.state.names.map((name) =>
+        {names.map((name) =>
           <Welcome name={name} />
         )}
       </div>
